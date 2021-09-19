0c387b2066c8547384539bec9634b59e3898d1f3
stroyerr <55282976+stroyerr@users.noreply.github.com>
2021-09-19 16:52:55 +1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 // JavaScript Document
+
+function toggleVisibleStaff(){
+	var x = document.getElementById("staffApp");
+	hideBuilder();
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideBuilder(){
+	document.getElementById("builderApp").style.display = "none";
+}
+
+function toggleVisibleBuilder(){
+	var x = document.getElementById("builderApp");
+	hideStaff();
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hideStaff(){
+	document.getElementById("staffApp").style.display = "none";
+}
